8bdc0f2897677e0e91a09ea8683222da84ce7b73
FelipeVas95 <61802591+FelipeVas95@users.noreply.github.com>
2021-07-07 09:34:56 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php  //Conexion entre interfaz y Base de datos
- If(!empty($_POST)){//verifica que el formulario halla sido enviado con datos diligenciados
-$serverName="PIPEV\SQLEXPRESS";//nombre del servidor de base de datos
-$connectionInfo=array ("Database"=>"Prueba");// nombre de la base de datos
-$con=sqlsrv_connect($serverName,$connectionInfo);//funcion para realizar la conexion
- }
-?>
